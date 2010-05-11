57f24f62368b7a73c42c408c6a64d23a76f2ca99
Henry Addo <addhen@linuxmail.org>
2010-05-11 07:30:16 +0000

    Added license details to the files

--- gen/org/addhen/smssync/R.java
+++ gen/org/addhen/smssync/R.java
@@ -17,7 +17,10 @@ public final class R {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f050001;
+        public static final int hello=0x7f050000;
+    }
+    public static final class xml {
+        public static final int preferences=0x7f040000;
     }
 }
