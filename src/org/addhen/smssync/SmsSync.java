8086960e4242f5e67f5987e82a2023baece5095e
Henry Addo <addhen@linuxmail.org>
2010-05-11 07:00:12 +0000

    Initial setup

---
+++ src/org/addhen/smssync/SmsSync.java
@@ -0,0 +1,13 @@
+package org.addhen.smssync;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SmsSync extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
