06328502b19163d653f500c8bcb8738f15fbbd02
eyedol <addhen@linuxmail.org>
2011-11-17 16:44:44 +0400

    Closes #2396 - Fix missing title
    The title for entering a URL at settings screen is missing. Added an
    existing title to the URL dialog

--- src/org/addhen/smssync/net/MainHttpClient.java
+++ src/org/addhen/smssync/net/MainHttpClient.java
@@ -241,7 +241,7 @@ public class MainHttpClient {
     private static String formatDate(String date) {
         try {
            
-            return Util.formatDateTime(Long.parseLong(date), "MM-dd-yy-hh:mm");
+            return Util.formatDateTime(Long.parseLong(date), "MM-dd-yy kk:mm");
         } catch (NumberFormatException e) {
             return null;
         }
