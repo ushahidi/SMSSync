c3ffbeaa9183742626276b8c2df8a60006c29ea9
eyedol <addhen@linuxmail.org>
2012-01-30 22:21:08 +0300

    Close #8 - Implement tool for generating smssync web pages
    Reads in markdown files and generates a fine html pages

--- tools/build_site.py
+++ tools/build_site.py
@@ -34,49 +34,84 @@ import csv
 import os
 import sys
 import markdown
+import time
 
 from optparse import OptionParser
 
-#from markdown import markdown.extensions.nl2br
+from datetime import date
 
+#TODO:// figure out how to use markdown 
 class BuildSite:
     
     def __init__(self,source,destination):
         self.source = source
         self.destination = destination
+        self.parts_of_webpages = '../genwebsite/'
 
     def transform_markdown_from_string(self,mkdown):
-        print markdown.markdown(mkdown)
+        return markdown.markdown(mkdown)
 
     def transform_markdown_from_file(self,mkdown):
         input_file = codecs.open(mkdown, mode="r", encoding="utf-8")
         mkdown_text = input_file.read()
         
         return markdown.markdown(mkdown_text)
-    
+
     def write_html_to_file(self,html_file,html_string):
-        output_file = codecs.open(html_file,"w", encoding="utf-8",
-                errors="xmlcharrefreplace"
-        )
+        print "Writing file "+html_file
+        output_file = open(html_file,'w')
         output_file.write(html_string)
 
-    def read_html_files(self):
-        f = open(self.source,'r')
-        content = f.readLines()
+        print "Succesffully Done!"
+
+    def read_html_files(self,source):
+        f = open(source,'r')
+        content = f.read()
         f.close()
         
         return content
+
+    def build_pages(self):
+        """ build web pages for smssync.ushahidi.com """
+        
+        #read header html stuff
+        header = self.read_html_files(self.parts_of_webpages+'site_header.html')
+
+        #read html stuff for the main content
+        body = self.read_html_files(self.source)
+
+        #Get today's date
+        d = date.today()
+
+        #read footer html stuff
+        footer = self.apply_filter('#Date','Generated: '+d.strftime("%d-%m-%Y"),
+                self.read_html_files(self.parts_of_webpages+'site_footer.html'))
+        
+        #combine the different parts of the web page into a single one
+        #TODO:// figure a better way of doing this.
+
+        page = header + body + footer
+
+        mkdown = self.transform_markdown_from_string(page)
+        
+        #write content to a file
+        self.write_html_to_file(self.destination,mkdown)
+    
+    def apply_filter(self,keyword, word, content):
+        """ find and replace for stuff in the html tags """
+        return content.replace(keyword,word)
+
 
 def main(args,options,parser):
     if len(args) < 2:
         parser.print_usage()
         print "%s: " %(str(len(args)))
     else :
-        build_site = BuildSite(args[0],args[0])
+        build_site = BuildSite(args[0],args[1])
         if options.mkfile == True:
             build_site.transform_markdown_from_file(args)
         else:
-            build_site.transform_markdown_from_string(args[0])
+            build_site.build_pages()
 usage = "usage: %prog <markdown string> <destination file>"
 
 parser = OptionParser(usage=usage)
