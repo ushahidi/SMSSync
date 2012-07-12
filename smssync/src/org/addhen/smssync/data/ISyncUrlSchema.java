f765681bed047a7e106b623b66fb72753287a75d
eyedol <addhen@linuxmail.org>
2012-07-12 08:26:45 +0000

    Add syncurl database schema

---
+++ smssync/src/org/addhen/smssync/data/ISyncUrlSchema.java
@@ -0,0 +1,28 @@
+package org.addhen.smssync.data;
+
+public interface ISyncUrlSchema {
+	
+	public static final String ID = "_id";
+
+	public static final String TITLE = "title";
+
+	public static final String KEYWORDS = "keywords";
+
+	public static final String STATUS = "status";
+	
+	public static final String TABLE = "syncurl";
+
+	public static final String[] SYNCURL_COLUMNS = new String[] { ID,
+			TITLE, KEYWORDS, STATUS};
+	
+	public static final String TABLE_CREATE = "CREATE TABLE IF NOT EXISTS "
+			+ TABLE
+			+ " ("
+			+ ID
+			+ " INTEGER PRIMARY KEY AUTOINCREMENT, "
+			+ STATUS
+			+ " INTEGER , "
+			+ KEYWORDS
+			+ " TEXT NOT NULL, "
+			+ TITLE + " TEXT" + ")";
+}
---
+++ smssync/src/org/addhen/smssync/entities/SyncUrl.java
@@ -0,0 +1,5 @@
+package org.addhen.smssync.entities;
+
+public class SyncUrl {
+
+}
