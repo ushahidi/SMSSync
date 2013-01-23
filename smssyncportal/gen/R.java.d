c94d9409e7bcb00fa237dd0437a73fb7492a6c07
Vaneyck <vaneycksemeotoki@gmail.com>
2013-01-23 20:25:11 +0300

    added .gitignore for sms-portal

--- smssyncportal/gen/R.java.d
+++
@@ -1,4 +0,0 @@
-/home/vaneyck/workspace/smssync/SMSSync/smssyncportal/gen/com/smssync/portal/R.java \
- : /home/vaneyck/workspace/smssync/SMSSync/smssyncportal/res/layout/main.xml \
-/home/vaneyck/workspace/smssync/SMSSync/smssyncportal/res/values/strings.xml \
-/home/vaneyck/workspace/smssync/SMSSync/smssyncportal/bin/AndroidManifest.xml \
