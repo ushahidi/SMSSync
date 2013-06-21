4a20bd4cc2abd62ddaba7f61edd8e6ae2d48689b
Henry Addo <addhen@linuxmail.org>
2013-06-21 16:37:46 +0900

    Add inline documentation to state class

--- smssync/src/org/addhen/smssync/tasks/state/State.java
+++ smssync/src/org/addhen/smssync/tasks/state/State.java
@@ -9,6 +9,11 @@ import org.addhen.smssync.exceptions.I8nException;
 
 import android.content.res.Resources;
 
+/**
+ * Base class to provide state of a running task
+ * 
+ * @author eyedol
+ */
 public abstract class State {
 
     public final SyncState state;
@@ -17,6 +22,13 @@ public abstract class State {
 
     public final MessageType messageType;
 
+    /**
+     * Default constructor
+     * 
+     * @param state The state of the task
+     * @param messageType The type of message being processed
+     * @param exception Exception to throw.
+     */
     public State(SyncState state, MessageType messageType, Exception exception) {
 
         this.state = state;
@@ -50,17 +62,34 @@ public abstract class State {
         return state == SyncState.CANCELED_SYNC;
     }
 
+    /**
+     * Get any error state of the task under execution.
+     * 
+     * @param resources
+     * @return
+     */
     public String getError(Resources resources) {
+        // exception shouldn't be null
         if (exception == null)
             return null;
 
+        // Is the exception message localizable
         if (exception instanceof I8nException) {
+            // return any localizable string
             return resources.getString(((I8nException) exception).resId());
         } else {
+            // return an exception message based on the active locale of the
+            // system
             return exception.getLocalizedMessage();
         }
     }
-
+    
+    /**
+     * Pass notification messages to the main UI
+     * 
+     * @param resources
+     * @return
+     */
     public String getNotificationMessage(Resources resources) {
         switch (state) {
             case ERROR:
