fd2b5eb0b5f47579de62a1f94ffec7859de2c0c6
Henry Addo <addhen@linuxmail.org>
2013-06-21 16:51:54 +0900

    Add inline documentation

--- smssync/src/org/addhen/smssync/tasks/state/MessageSyncState.java
+++ smssync/src/org/addhen/smssync/tasks/state/MessageSyncState.java
@@ -1,3 +1,22 @@
+/*****************************************************************************
+ ** Copyright (c) 2010 - 2012 Ushahidi Inc
+ ** All rights reserved
+ ** Contact: team@ushahidi.com
+ ** Website: http://www.ushahidi.com
+ **
+ ** GNU Lesser General Public License Usage
+ ** This file may be used under the terms of the GNU Lesser
+ ** General Public License version 3 as published by the Free Software
+ ** Foundation and appearing in the file LICENSE.LGPL included in the
+ ** packaging of this file. Please review the following information to
+ ** ensure the GNU Lesser General Public License version 3 requirements
+ ** will be met: http://www.gnu.org/licenses/lgpl.html.
+ **
+ **
+ ** If you have questions regarding the use of this file, please contact
+ ** Ushahidi developers at team@ushahidi.com.
+ **
+ *****************************************************************************/
 
 package org.addhen.smssync.tasks.state;
 
@@ -25,6 +44,9 @@ public class MessageSyncState extends State {
         this.syncType = syncType;
     }
 
+    /**
+     * Create default state
+     */
     public MessageSyncState() {
         this(INITIAL, 0, 0, UNKNOWN, null, null);
     }
@@ -36,6 +58,12 @@ public class MessageSyncState extends State {
                 exception);
     }
 
+    /**
+     * Get the notification
+     * 
+     * @param resources
+     * @return
+     */
     public String getNotification(Resources resources) {
 
         String msg = super.getNotificationMessage(resources);
@@ -55,7 +83,7 @@ public class MessageSyncState extends State {
 
     @Override
     public String toString() {
-        return "BackupStateChanged[" +
+        return "SyncStateChanged[" +
                 "state=" + state +
                 ", currentSyncedItems=" + currentSyncedItems +
                 ", itemsToSync=" + itemsToSync +
