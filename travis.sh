173c0d7e78f8e40523d7f82828cd5df46ef27e8c
Henry Addo <addhen@linuxmail.org>
2013-07-21 13:34:29 +0900

    Attempt to get travis ci to work again

---
+++ travis.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+# Use by travic CI --https://travis-ci.org/ushahidi/SMSSync to build SMSSync 
+
+function log {
+	local logStart="#"
+	local sourceLen=${#BASH_SOURCE[@]}
+	for ((i=$sourceLen-1; i>0; --i)); do
+		logStart="$logStart [$(basename ${BASH_SOURCE[$i]})]"
+	done
+	echo "$logStart $@"
+}
+
+log "Building smssync..."
+pushd smssync
+ant clean debug
+popd
+log "Smssync built."
+
+log "Building test app..."
+pushd smssync/tests
+ant clean build-project
+popd
+log "Test app built."
+
+log "BUILD COMPLETE"
+
