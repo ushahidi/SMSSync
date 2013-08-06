4a845380644506c7fa42ef172e6a6642a7815e00
Henry Addo <addhen@linuxmail.org>
2013-08-07 07:55:55 +0900

    Add menu icons

---
+++ smssync/src/main/java/org/addhen/smssync/navdrawer/DonationNavDrawerItem.java
@@ -0,0 +1,50 @@
+/*****************************************************************************
+ ** Copyright (c) 2010 - 2012 Ushahidi Inc
+ ** All rights reserved
+ ** Contact: team@ushahidi.com
+ ** Website: http://www.ushahidi.com
+ **
+ ** GNU Lesser General Public License Usage
+ ** This file may be used under the terms of the GNU Lesser
+ ** General Public License version 3 as published by the Free Software
+ ** Foundation and appearing in the file LICENSE.LGPL included in the
+ ** packaging of this file. Please review the following information to
+ ** ensure the GNU Lesser General Public License version 3 requirements
+ ** will be met: http://www.gnu.org/licenses/lgpl.html.
+ **
+ **
+ ** If you have questions regarding the use of this file, please contact
+ ** Ushahidi developers at team@ushahidi.com.
+ **
+ *****************************************************************************/
+
+package org.addhen.smssync.navdrawer;
+
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+
+/**
+ * Donation nav drawer item
+ */
+public class DonationNavDrawerItem extends BaseNavDrawerItem {
+
+    /**
+     *
+     * @param title
+     * @param iconRes
+     * @param activity
+     */
+    public DonationNavDrawerItem(String title, int iconRes,
+            SherlockFragmentActivity activity) {
+        super(title, iconRes, activity);
+    }
+
+    @Override
+    protected void onSelectItem() {
+
+    }
+
+    @Override
+    public void setCounter() {
+
+    }
+}
