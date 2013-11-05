b3b15b6027044c31d3c265c488d22a78f185ae0e
Henry Addo <addhen@linuxmail.org>
2013-11-05 11:32:26 +0900

    Remove method redundant call

--- smssync/src/main/java/org/addhen/smssync/services/SyncPendingMessagesService.java
+++ smssync/src/main/java/org/addhen/smssync/services/SyncPendingMessagesService.java
@@ -131,11 +131,6 @@ public class SyncPendingMessagesService extends BaseService {
         return mState;
     }
 
-    @Produce
-    public boolean readLogs() {
-        return true;
-    }
-
     private void updateSyncStatusNotification(SyncPendingMessagesState state) {
         createNotification(R.string.sync_in_progress,
                 state.getNotification(getResources()), getPendingIntent());
