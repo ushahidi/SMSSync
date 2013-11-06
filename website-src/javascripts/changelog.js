4d6842470e84af6aa8e6c253c301b1a4d2945977
Henry Addo <addhen@linuxmail.org>
2013-11-06 13:27:52 +0900

    This time changelog json file validates

--- website-src/javascripts/changelog.js
+++ website-src/javascripts/changelog.js
@@ -1,23 +1,15 @@
 
-$( "#latest-header" ).load( "https://raw.github.com/ushahidi/SMSSync/develop/CREDITS" );
-$.get('../CREDITS', function(data) {
-      $("#latest-header").html(data);
-    }, 'text');
 var url = "http://query.yahooapis.com/v1/public/yql?q=select * from json where url='https://raw.github.com/ushahidi/SMSSync/develop/changelog.json'&format=json";
 $.ajax({
     url: url,
     cache : false,
     dataType : 'jsonp',
-    success : fetchHistory
+    success : fetchChangelog
 });
 
 // Get history details
-function fetchHistory(data) {
-    var histories = data.query.results.json.json;
-    $('#latest-header').html(Mustache.render($('#latest-header').html(), {"latest" : histories[0] }));
-    $('#versions').html(Mustache.render($('#versions').html(), {"versions" : histories}));
-    $.each(histories, function(index,history){
-        //$('#latest-header').html(Mustache.render($('#latest-header').html(), {"latest" : versions[0] }));
-      //$('#versions').html(Mustache.render($('#versions').html(), {"versions" : versions}));
-    });
+function fetchChangelog(data) {
+    var changelogs = data.query.results.json.json;
+    $('#latest-header').html(Mustache.render($('#latest-header').html(), {"latest" : changelogs[0] }));
+    $('#versions').html(Mustache.render($('#versions').html(), {"versions" : changelogs}));
 }
