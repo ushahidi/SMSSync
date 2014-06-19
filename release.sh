1b897beffa40ff8615cdffaa200cc2b9bd30610f
Henry Addo <addhen@linuxmail.org>
2014-06-19 19:26:01 +0900

    Update script for making a release

--- release.sh
+++ release.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Make a release to the SMSSync app.
+# TODO:: Look into making this a Gradle task
 
 if [[ $# -lt 2 ]]
 then
@@ -12,53 +13,28 @@ TAG_NAME=$1
 TAG_MESSAGE=$2
 DEVELOP='develop'
 MASTER='master'
-RELEASE='release'
 echo $TAG_MESSAGE
 
 # SMSSync source code
 SMSSYNC='smssync'
 
-# Merge release to master and develop
-
-# Checkout develop branch
-echo "Checking out the develop branch..."
-git checkout $DEVELOP
-
-# Merge release branch into the develop branch
-echo "Merging release branch into develop..."
-git merge $RELEASE
+# Merge develop branch into master
 
 # Checkout master branch
 echo "Checking out the master branch..."
 git checkout $MASTER
 
-# Merge release branch into master branch
-echo "Merging release branch into"
-git merge $RELEASE
+# Merge develop branch into master branch
+echo "Merging develop branch into master"
+git merge $DEVELOP
 
-# Delete the release branch
-echo "Deleting local release branch..."
-git branch -d $RELEASE
-
-# Delete remote release branch
-echo "Deleting remote release branch..."
-git push origin --delete $RELEASE 
-
-# Create the release tag
+# Create the signed release tag
 echo "Creating release tag $TAG_NAME ..."
-git tag -a $TAG_NAME -m $TAG_MESSAGE
-
-# Push newly created tag to remote host
-echo "Pushing new tag to remote host..."
-git push --tags
-
-# Create the release build
-echo "Changing directory into $SMSSYNC"
-pushd $SMSSYNC
+git tag -s $TAG_NAME -m $TAG_MESSAGE
 
 # Create a release apk
 echo "Building a release apk"
-ant clean release
+./gradlew clean assemble
 
 # Back to where we were before
 popd
