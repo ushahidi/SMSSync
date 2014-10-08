05262747b575a22dc73ad147911b7c69ec0b9208
Henry Addo <addhen@linuxmail.org>
2014-10-08 16:13:37 +0900

    Implement URI helper class for posting to email address to Google docs

--- survey-dialog/src/main/java/org/addhen/smssync/survey/dialog/UriHelper.java
+++ survey-dialog/src/main/java/org/addhen/smssync/survey/dialog/UriHelper.java
@@ -1,11 +1,15 @@
 package org.addhen.smssync.survey.dialog;
 
+import android.content.Context;
 import android.net.Uri;
 
 /**
+ *
  * @author Ushahidi Team <team@ushahidi.com>
  */
 public interface UriHelper {
 
-    Uri getUri(String packageName);
+    Uri getUri();
+
+    void goToUri(Context context);
 }
--- survey-dialog/src/main/java/org/addhen/smssync/survey/dialog/UriHelperImpl.java
+++ survey-dialog/src/main/java/org/addhen/smssync/survey/dialog/UriHelperImpl.java
@@ -1,20 +1,24 @@
 package org.addhen.smssync.survey.dialog;
 
+import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 
 public class UriHelperImpl implements UriHelper {
 
-    private static final String GOOGLE_PLAY = "https://play.google.com/store/apps/details?id=";
+    private static final String GOOGLE_SPREAD_SHEET_URL
+            = "https://play.google.com/store/apps/details?id=";
 
     public UriHelperImpl() {
     }
 
-    static Uri getGooglePlay(String packageName) {
-        return packageName == null ? null : Uri.parse(GOOGLE_PLAY + packageName);
+    public void goToUri(Context context) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, getUri());
+        context.startActivity(intent);
     }
 
     @Override
-    public Uri getUri(String packageName) {
-        return packageName == null ? null : Uri.parse(GOOGLE_PLAY + packageName);
+    public Uri getUri() {
+        return Uri.parse(GOOGLE_SPREAD_SHEET_URL);
     }
 }
