7ca856d2cc55e48bab53b658f0e215d8e1e714fe
Henry Addo <addhen@linuxmail.org>
2015-01-08 12:12:46 +0900

    Remove debug comment

--- smssync/src/main/java/org/addhen/smssync/net/MessageSyncHttpClient.java
+++ smssync/src/main/java/org/addhen/smssync/net/MessageSyncHttpClient.java
@@ -70,13 +70,13 @@ public class MessageSyncHttpClient extends MainHttpClient {
         setHeader("Content-Type", syncScheme.getContentType());
         addParam(syncScheme.getKey(SyncDataKey.SECRET), syncUrl.getSecret());
         addParam(syncScheme.getKey(SyncDataKey.FROM), message.getFrom());
-        /*addParam(syncScheme.getKey(SyncDataKey.MESSAGE), message.getBody());
+        addParam(syncScheme.getKey(SyncDataKey.MESSAGE), message.getBody());
         addParam(
                 syncScheme.getKey(SyncDataKey.SENT_TIMESTAMP), message.getTimestamp()
         );
         addParam(syncScheme.getKey(SyncDataKey.SENT_TO), toNumber);
         addParam(syncScheme.getKey(SyncDataKey.MESSAGE_ID), message.getUuid());
-        addParam(syncScheme.getKey(SyncDataKey.DEVICE_ID), deviceId);*/
+        addParam(syncScheme.getKey(SyncDataKey.DEVICE_ID), deviceId);
         try {
             setHttpEntity(format);
         } catch (Exception e) {
