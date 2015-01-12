3ed36ac11fc64f9d46c1eaa07a92c114eff51534
Henry Addo <addhen@linuxmail.org>
2015-01-12 15:00:12 +0900

    Updating webiste...

---
+++ trying.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Make a release to the SMSSync app.
+# TODO:: Look into making this a Gradle task
+
+TMP_DIR='/tmp/website-src'
+
+# cd into the website folder
+echo "Compiling website"
+pushd website-src
+
+ruhoh  compile $TMP_DIR
+cp CNAME $TMP_DIR
+popd
+
+pushd $TMP_DIR
+git init
+
+git add .
+
+git commit -m "Updating webiste..."
+
+popd
+
+echo "Done!"
