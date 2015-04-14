6e9060d85423fd9caf9cf1cfb1e0a56bef179502
Henry Addo <addhen@linuxmail.org>
2015-04-15 06:42:43 +0900

    Minor code clean up

--- smssync/src/main/java/org/addhen/smssync/util/JsonUtils.java
+++ smssync/src/main/java/org/addhen/smssync/util/JsonUtils.java
@@ -19,8 +19,6 @@ package org.addhen.smssync.util;
 
 import com.google.gson.Gson;
 
-import java.lang.reflect.Type;
-
 /**
  * Created by Kamil Kalfas(kkalfas@soldevelo.com) on 24.04.14.
  * <p/>
@@ -30,14 +28,6 @@ public final class JsonUtils {
 
     private JsonUtils() {
         // Prevent instantiation
-    }
-
-    public static <T> T getObj(String json, java.lang.Class<T> afterClass) {
-        return new Gson().fromJson(json, afterClass);
-    }
-
-    public static <T> T getObj(String json, Type type) {
-        return new Gson().fromJson(json, type);
     }
 
     public static String objToJson(Object object) {
--- smssync/src/main/java/org/addhen/smssync/util/ServicesConstants.java
+++ smssync/src/main/java/org/addhen/smssync/util/ServicesConstants.java
@@ -19,24 +19,13 @@ package org.addhen.smssync.util;
 
 public class ServicesConstants {
 
-    public static final int ACTIVE_SYNC_URL = 1;
-    public static final int INACTIVE_SYNC_URL = 1;
-    public static final String DEFAULT_SMS_PROVIDER = "org.addhen.smssync.defaultsmsprovider";
     public static final String SENT_SMS_BUNDLE = "sent";
     public static final String DELIVERED_SMS_BUNDLE = "delivered";
-    public static int CHECK_TASK_SERVICE_REQUEST_CODE = 0;
     public static int CHECK_TASK_SCHEDULED_SERVICE_REQUEST_CODE = 1;
-    public static int AUTO_SYNC_SERVICE_REQUEST_CODE = 2;
     public static int AUTO_SYNC_SCHEDULED_SERVICE_REQUEST_CODE = 3;
     public static int MESSAGE_RESULTS_SCHEDULED_SERVICE_REQUEST_CODE = 4;
     public static String AUTO_SYNC_ACTION = "org.addhen.smssync.syncservices.autosync";
-    public static String CHECT_TASK_ACTION = "org.addhen.smssync.syncservices.checktask";
-    public static String AUTO_SYNC_SCHEDULED_ACTION
-            = "org.addhen.smssync.syncservices.autosyncscheduled";
-    public static String CHECT_TASK_SCHEDULED_ACTION
-            = "org.addhen.smssync.syncservices.checktaskscheduled";
     public static String FAILED_ACTION = "org.addhen.smssync.syncservices.failed";
-    public static String BATTERY_LEVEL = "org.addhen.smssync.syncservices.batterylevel";
     public static String MESSAGE_UUID = "message_uuid";
     public static String SENT = "SMS_SENT";
     public static String DELIVERED = "SMS_DELIVERED";
