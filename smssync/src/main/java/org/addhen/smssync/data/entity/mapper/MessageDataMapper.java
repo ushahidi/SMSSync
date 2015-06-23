50ac8d686b746dde0fb0f43bb28365cb08a45ece
Henry Addo <addhen@linuxmail.org>
2015-06-23 11:31:51 +0900

    Implement message data mapper

--- smssync/src/main/java/org/addhen/smssync/data/entity/mapper/MessageDataMapper.java
+++ smssync/src/main/java/org/addhen/smssync/data/entity/mapper/MessageDataMapper.java
@@ -17,9 +17,79 @@
 
 package org.addhen.smssync.data.entity.mapper;
 
+import org.addhen.smssync.data.entity.Message;
+import org.addhen.smssync.domain.entity.MessageEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
 /**
  * @author Ushahidi Team <team@ushahidi.com>
  */
 public class MessageDataMapper {
 
+    @Inject
+    public MessageDataMapper() {
+        // Do nothing
+    }
+
+    public MessageEntity map(Message message) {
+        MessageEntity messageEntity = null;
+        if (message != null) {
+            messageEntity = new MessageEntity();
+            messageEntity._id = message._id;
+            messageEntity.messageBody = message.messageBody;
+            messageEntity.messageDate = message.messageDate;
+            messageEntity.messageFrom = message.messageFrom;
+            messageEntity.messageUuid = message.messageUuid;
+            messageEntity.sentResultMessage = message.sentResultMessage;
+            messageEntity.sentResultCode = message.sentResultCode;
+            messageEntity.messageType = MessageEntity.Type.valueOf(message.messageType.name());
+            messageEntity.status = MessageEntity.Status.valueOf(message.status.name());
+            messageEntity.deliveryResultCode = message.deliveryResultCode;
+            messageEntity.deliveryResultMessage = message.deliveryResultMessage;
+        }
+        return messageEntity;
+    }
+
+    public Message map(MessageEntity messageEntity) {
+        Message message = null;
+        if (message != null) {
+            message = new Message();
+            message._id = messageEntity._id;
+            message.messageBody = messageEntity.messageBody;
+            message.messageDate = messageEntity.messageDate;
+            message.messageFrom = messageEntity.messageFrom;
+            message.messageUuid = messageEntity.messageUuid;
+            message.sentResultMessage = messageEntity.sentResultMessage;
+            message.sentResultCode = messageEntity.sentResultCode;
+            message.messageType = Message.Type.valueOf(messageEntity.messageType.name());
+            message.status = Message.Status.valueOf(messageEntity.status.name());
+            message.deliveryResultCode = messageEntity.deliveryResultCode;
+            message.deliveryResultMessage = messageEntity.deliveryResultMessage;
+        }
+        return message;
+    }
+
+    public List<MessageEntity> map(List<Message> messageList) {
+        List<MessageEntity> messageEntityList = new ArrayList<>();
+        MessageEntity messageEntity;
+        for (Message message : messageList) {
+            messageEntity = map(message);
+            if (messageEntity != null) {
+                messageEntityList.add(messageEntity);
+            }
+        }
+        return messageEntityList;
+    }
+
+    public Message.Status map(MessageEntity.Status status) {
+        return Message.Status.valueOf(status.name());
+    }
+
+    public Message.Type map(MessageEntity.Type type) {
+        return Message.Type.valueOf(type.name());
+    }
 }
