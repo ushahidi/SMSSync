da17cfcf9df76aa6d8904a66eea82035e8769407
Henry Addo <addhen@linuxmail.org>
2015-09-10 10:15:13 +0900

    Use new API from the design library to implement scroll aware fab behaviour

--- smssync/src/main/java/org/addhen/smssync/presentation/view/ui/widget/FabBehavior.java
+++ smssync/src/main/java/org/addhen/smssync/presentation/view/ui/widget/FabBehavior.java
@@ -18,26 +18,16 @@
 package org.addhen.smssync.presentation.view.ui.widget;
 
 import android.content.Context;
-import android.os.Build;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.view.ViewCompat;
-import android.support.v4.view.ViewPropertyAnimatorListener;
-import android.support.v4.view.animation.FastOutSlowInInterpolator;
 import android.util.AttributeSet;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.view.animation.Interpolator;
 
 /**
  * @author Henry Addo
  */
 public class FabBehavior extends FloatingActionButton.Behavior {
-
-    private static final Interpolator INTERPOLATOR = new FastOutSlowInInterpolator();
-
-    private boolean mIsAnimatingOut = false;
 
     public FabBehavior(Context context, AttributeSet attrs) {
         super();
@@ -46,12 +36,11 @@ public class FabBehavior extends FloatingActionButton.Behavior {
     @Override
     public boolean onStartNestedScroll(final CoordinatorLayout coordinatorLayout,
             final FloatingActionButton child,
-            final View directTargetChild,
-            final View target, final int nestedScrollAxes) {
+            final View directTargetChild, final View target, final int nestedScrollAxes) {
         // Ensure we react to vertical scrolling
         return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL
-                || super.onStartNestedScroll(coordinatorLayout,
-                child, directTargetChild, target, nestedScrollAxes);
+                || super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target,
+                nestedScrollAxes);
     }
 
     @Override
@@ -59,81 +48,14 @@ public class FabBehavior extends FloatingActionButton.Behavior {
             final FloatingActionButton child,
             final View target, final int dxConsumed, final int dyConsumed,
             final int dxUnconsumed, final int dyUnconsumed) {
-
-        super.onNestedScroll(coordinatorLayout, child, target,
-                dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
-
-        if (dyConsumed > 0 && !this.mIsAnimatingOut && child.getVisibility() == View.VISIBLE) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed,
+                dyUnconsumed);
+        if (dyConsumed > 0 && child.getVisibility() == View.VISIBLE) {
             // User scrolled down and the FAB is currently visible -> hide the FAB
-            animateOut(child);
+            child.hide();
         } else if (dyConsumed < 0 && child.getVisibility() != View.VISIBLE) {
             // User scrolled up and the FAB is currently not visible -> show the FAB
-            animateIn(child);
-        }
-    }
-
-    // Same animation that FloatingActionButton.Behavior uses to hide the FAB when the AppBarLayout exits
-    private void animateOut(final FloatingActionButton button) {
-        CoordinatorLayout.LayoutParams lp = (CoordinatorLayout.LayoutParams) button
-                .getLayoutParams();
-        int fabBottom = lp.bottomMargin;
-
-        if (Build.VERSION.SDK_INT >= 14) {
-            ViewCompat.animate(button).setDuration(300L)
-                    .translationY(button.getHeight() + fabBottom)
-                    .setInterpolator(INTERPOLATOR)
-                    .setListener(new ViewPropertyAnimatorListener() {
-                        public void onAnimationStart(View view) {
-                            FabBehavior.this.mIsAnimatingOut = true;
-                        }
-
-                        public void onAnimationCancel(View view) {
-                            FabBehavior.this.mIsAnimatingOut = false;
-                        }
-
-                        public void onAnimationEnd(View view) {
-                            FabBehavior.this.mIsAnimatingOut = false;
-                            view.setVisibility(View.GONE);
-                        }
-                    }).start();
-        } else {
-            Animation anim = AnimationUtils
-                    .loadAnimation(button.getContext(), android.R.anim.fade_out);
-            anim.setInterpolator(INTERPOLATOR);
-            anim.setDuration(300L);
-            anim.setAnimationListener(new Animation.AnimationListener() {
-                public void onAnimationStart(Animation animation) {
-                    FabBehavior.this.mIsAnimatingOut = true;
-                }
-
-                public void onAnimationEnd(Animation animation) {
-                    FabBehavior.this.mIsAnimatingOut = false;
-                    button.setVisibility(View.GONE);
-                }
-
-                @Override
-                public void onAnimationRepeat(final Animation animation) {
-                }
-            });
-            button.startAnimation(anim);
-        }
-    }
-
-    // Same animation that FloatingActionButton.Behavior uses to show the FAB when the AppBarLayout enters
-    private void animateIn(FloatingActionButton button) {
-
-        button.setVisibility(View.VISIBLE);
-        if (Build.VERSION.SDK_INT >= 14) {
-            ViewCompat.animate(button).setDuration(300L).translationY(0.0F)
-                    .scaleX(1.0F).scaleY(1.0F).alpha(1.0F)
-                    .setInterpolator(INTERPOLATOR).withLayer().setListener(null)
-                    .start();
-        } else {
-            Animation anim = AnimationUtils
-                    .loadAnimation(button.getContext(), android.R.anim.fade_in);
-            anim.setDuration(300L);
-            anim.setInterpolator(INTERPOLATOR);
-            button.startAnimation(anim);
+            child.show();
         }
     }
 }
