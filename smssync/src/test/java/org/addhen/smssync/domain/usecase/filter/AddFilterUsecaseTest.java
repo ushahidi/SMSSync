aeeb8384f7aee61dd86496ea5ade870ba7a6afc1
Henry Addo <addhen@linuxmail.org>
2015-10-04 22:35:59 +0900

    Add test for add filter use case

---
+++ smssync/src/test/java/org/addhen/smssync/domain/usecase/filter/AddFilterUsecaseTest.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2010 - 2015 Ushahidi Inc
+ * All rights reserved
+ * Contact: team@ushahidi.com
+ * Website: http://www.ushahidi.com
+ * GNU Lesser General Public License Usage
+ * This file may be used under the terms of the GNU Lesser
+ * General Public License version 3 as published by the Free Software
+ * Foundation and appearing in the file LICENSE.LGPL included in the
+ * packaging of this file. Please review the following information to
+ * ensure the GNU Lesser General Public License version 3 requirements
+ * will be met: http://www.gnu.org/licenses/lgpl.html.
+ *
+ * If you have questions regarding the use of this file, please contact
+ * Ushahidi developers at team@ushahidi.com.
+ */
+
+package org.addhen.smssync.domain.usecase.filter;
+
+import com.addhen.android.raiburari.domain.executor.PostExecutionThread;
+import com.addhen.android.raiburari.domain.executor.ThreadExecutor;
+
+import org.addhen.smssync.domain.entity.DomainEntityFixture;
+import org.addhen.smssync.domain.entity.FilterEntity;
+import org.addhen.smssync.domain.repository.FilterRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.truth.Truth.assert_;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.verifyZeroInteractions;
+
+/**
+ * @author Ushahidi Team <team@ushahidi.com>
+ */
+public class AddFilterUsecaseTest {
+
+    private AddFilterUsecase mAddFilterUsecase;
+
+    @Mock
+    private ThreadExecutor mMockThreadExecutor;
+
+    @Mock
+    private PostExecutionThread mMockPostExecutionThread;
+
+    @Mock
+    private FilterRepository mMockFilterRepository;
+
+    @Mock
+    private FilterEntity mFilterEntity;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        mAddFilterUsecase = new AddFilterUsecase(mMockFilterRepository, mMockThreadExecutor,
+                mMockPostExecutionThread);
+    }
+
+    @Test
+    public void shouldSuccessfullyAddAFilter() {
+        final FilterEntity filterEntity = DomainEntityFixture.getFilterEntity();
+        mAddFilterUsecase.setFilter(filterEntity);
+        mAddFilterUsecase.buildUseCaseObservable();
+        verify(mMockFilterRepository).addEntity(filterEntity);
+
+        verifyNoMoreInteractions(mMockFilterRepository);
+        verifyZeroInteractions(mMockPostExecutionThread);
+        verifyZeroInteractions(mMockThreadExecutor);
+    }
+
+    @Test
+    public void shouldThrowRuntimeException() {
+        assertThat(mAddFilterUsecase).isNotNull();
+        mAddFilterUsecase.setFilter(null);
+        try {
+            mAddFilterUsecase.execute(null);
+            assert_().fail("Should have thrown RuntimeException");
+        } catch (RuntimeException e) {
+            assertThat(e).hasMessage("FilterEntity is null. You must call setFilter(...)");
+        }
+    }
+}
