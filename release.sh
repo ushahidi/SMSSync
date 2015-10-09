199517210a7bb204c21e1e25bb0b541df17a8ea7
Henry Addo <addhen@linuxmail.org>
2015-10-09 13:11:08 +0900

    Remove the ability to merge master to develop branch from the release script as it doesn't exist anymore

--- release.sh
+++ release.sh
@@ -15,32 +15,20 @@ echo "Releasing..."
 
 TAG_NAME=$1
 TAG_MESSAGE=$2
-DEVELOP='develop'
 MASTER='master'
-TMP_DIR='/tmp/public'
 echo $TAG_MESSAGE
 
 # SMSSync source code
 SMSSYNC='smssync'
 
-# Merge develop branch into master
-
 # Checkout master branch
 echo "Checking out the master branch..."
 git checkout $MASTER
-
-# Merge develop branch into master branch
-echo "Merging develop branch into master"
-git merge $DEVELOP
 
 # Create a release apk
 echo "Building a release apk"
 ./gradlew clean
 
 ./gradlew release -Prelease.scope=$SCOPE -Prelease.stage=$STAGE -PuploadTrack=$TRACK
-
-# Checkout develop branch
-echo "Checking out develop branch..."
-git checkout $DEVELOP
 
 echo "Done!"
