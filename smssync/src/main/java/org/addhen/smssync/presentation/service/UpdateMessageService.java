e33c86270a081b6e0778f8574af40ea219a8a76a
Henry Addo <addhen@linuxmail.org>
2015-11-19 15:33:23 +0900

    Fix #354 - Server response
    Call method that sends SMS when a request is successfully made

--- smssync/src/main/java/org/addhen/smssync/presentation/service/UpdateMessageService.java
+++ smssync/src/main/java/org/addhen/smssync/presentation/service/UpdateMessageService.java
@@ -17,6 +17,7 @@
 
 package org.addhen.smssync.presentation.service;
 
+import org.addhen.smssync.presentation.model.MessageModel;
 import org.addhen.smssync.presentation.presenter.message.UpdateMessagePresenter;
 import org.addhen.smssync.presentation.view.message.UpdateMessageView;
 
@@ -64,14 +65,14 @@ public class UpdateMessageService extends BaseWakefulIntentService implements Up
         mDelayedStopHandler.sendEmptyMessageDelayed(0, STOP_DELAY);
         mDelayedStopHandler.removeCallbacksAndMessages(null);
         mServiceStarted = false;
-
+        // Release resources
     }
-    // Release resources
 
     @Override
     protected void executeTask(Intent intent) {
         mServiceStarted = true;
-        mUpdateMessagePresenter.updateMessage(null);
+        MessageModel messageModel = (MessageModel) intent.getParcelableExtra(ServiceConstants.UPDATE_MESSAGE);
+        mUpdateMessagePresenter.updateMessage(messageModel);
 
     }
 
